f52283b7cf998a836cf9983641712534fcc6a263
Murilo Henrique <murilodioliveira@gmail.com>
2022-06-15 13:48:58 -0300

    Adicionando README e primeiro desafio

---
+++ README.md
@@ -0,0 +1,8 @@
+## Desafios em Javascript
+
+Neste repositório estão listados todos os desafios concluídos ou em andamento realizados por mim em Javascript.
+
+# Em andamento
+    - Cálculo de Fatorial
+
+# Concluído
---
+++ fatorial.js
@@ -0,0 +1,12 @@
+function FirstFactorial(num) { 
+    for (let i = 0; i < num + 1; i++) {
+      const fatorial = [i];
+      console.table(fatorial);
+    }
+    // code goes here  
+    return num; 
+  
+  }
+     
+  // keep this function call here 
+  console.log(FirstFactorial(1));
