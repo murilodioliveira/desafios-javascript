7baa272ea1343f48f5f8e132bb1a720cdeaa09ad
Murilo Henrique <murilodioliveira@gmail.com>
2022-06-16 18:02:39 -0300

    Há um jeito melhor de fazer isso

--- contaPalavras.js
+++ contaPalavras.js
@@ -1,10 +1,19 @@
 // ##### Contador de Palavras #####
-/* Esse desafio tem por objetivo contar quantas palavras se repetem
-   em cada frase. */
+/* Esse desafio tem por objetivo contar quantas palavras se repetemem cada frase. */
 
-function contaPalavras(string){
+function contaPalavras(frase){
+    /*const retiraEspaco = frase.split(" ");
+    let fraseAlterada = retiraEspaco.join();
+    
+    const retiraExclamacao = fraseAlterada.split("!");
+    fraseAlterada = retiraExclamacao.join();
 
+    const retiraVirgula = fraseAlterada.split(",");
+    fraseAlterada = retiraVirgula;
+    console.log(fraseAlterada);
+    */  //Há uma forma melhor de fazer isso
 }
 
-const frase = "Filhos de Gondor! De Rohan! Meus Irmãos! Eu vejo em seus olhos o mesmo medo que tiraria o meu coração. Talvez possa haver o dia quando a coragem dos homens falhará, quando abandonaremos nossos amigos, e quebraremos todos os laços de sociedades, mas esse não é o dia! Uma hora de lobos e escudos partidos quando a Era do Homem cairá, mas esse não é o dia! Esse dia nós lutamos! Por todos aqueles que nos são queridos nesta boa terra, eu peço-vos que fiquem, Homens do Oeste!";
+let discurso = "Filhos de Gondor! De Rohan! Meus Irmãos! Eu vejo em seus olhos o mesmo medo que tiraria o meu coração. Talvez possa haver o dia quando a coragem dos homens falhará, quando abandonaremos nossos amigos, e quebraremos todos os laços de sociedades, mas esse não é o dia! Uma hora de lobos e escudos partidos quando a Era do Homem cairá, mas esse não é o dia! Esse dia nós lutamos! Por todos aqueles que nos são queridos nesta boa terra, eu peço-vos que fiquem, Homens do Oeste!";
 
+contaPalavras(discurso);
