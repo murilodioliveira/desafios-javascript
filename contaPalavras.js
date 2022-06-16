fa35c3801eabfb415a3bfe5b8846b20f323e59be
Murilo Henrique <murilodioliveira@gmail.com>
2022-06-16 14:39:55 -0300

    Inicio do desafio Contagem de Palvras

---
+++ contaPalavras.js
@@ -0,0 +1,10 @@
+// ##### Contador de Palavras #####
+/* Esse desafio tem por objetivo contar quantas palavras se repetem
+   em cada frase. */
+
+function contaPalavras(string){
+
+}
+
+const frase = "Filhos de Gondor! De Rohan! Meus Irmãos! Eu vejo em seus olhos o mesmo medo que tiraria o meu coração. Talvez possa haver o dia quando a coragem dos homens falhará, quando abandonaremos nossos amigos, e quebraremos todos os laços de sociedades, mas esse não é o dia! Uma hora de lobos e escudos partidos quando a Era do Homem cairá, mas esse não é o dia! Esse dia nós lutamos! Por todos aqueles que nos são queridos nesta boa terra, eu peço-vos que fiquem, Homens do Oeste!";
+
