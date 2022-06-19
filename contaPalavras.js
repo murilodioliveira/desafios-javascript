8dafab24238ddd35c64449928f70fa1bcf666946
Murilo Henrique <murilodioliveira@gmail.com>
2022-06-18 23:09:29 -0300

    Melhor resolução parcial do problema - Caminho certo

--- contaPalavras.js
+++ contaPalavras.js
@@ -1,19 +1,16 @@
 // ##### Contador de Palavras #####
 /* Esse desafio tem por objetivo contar quantas palavras se repetemem cada frase. */
 
-function contaPalavras(frase){
-    /*const retiraEspaco = frase.split(" ");
-    let fraseAlterada = retiraEspaco.join();
-    
-    const retiraExclamacao = fraseAlterada.split("!");
-    fraseAlterada = retiraExclamacao.join();
-
-    const retiraVirgula = fraseAlterada.split(",");
-    fraseAlterada = retiraVirgula;
-    console.log(fraseAlterada);
-    */  //Há uma forma melhor de fazer isso
+function contaPalavras(string){
+    let arrumaFrase = string.replace(/\W/gm, " "); // Substitui todas as pontuações e acentuações
+    let fraseSeparada = arrumaFrase.split(/\s+/gm);
+    if (fraseSeparada[fraseSeparada.length - 1] == "") {
+        fraseSeparada.pop();        
+    }
+    console.log(fraseSeparada);
+    console.log(fraseSeparada.length);
 }
 
-let discurso = "Filhos de Gondor! De Rohan! Meus Irmãos! Eu vejo em seus olhos o mesmo medo que tiraria o meu coração. Talvez possa haver o dia quando a coragem dos homens falhará, quando abandonaremos nossos amigos, e quebraremos todos os laços de sociedades, mas esse não é o dia! Uma hora de lobos e escudos partidos quando a Era do Homem cairá, mas esse não é o dia! Esse dia nós lutamos! Por todos aqueles que nos são queridos nesta boa terra, eu peço-vos que fiquem, Homens do Oeste!";
+let frase = "Ao surgir, a! manha!";
 
-contaPalavras(discurso);
+contaPalavras(frase);
