8cde1c53cec54290f7518fac1521156517f878cb
Murilo Henrique <murilodioliveira@gmail.com>
2022-06-18 23:46:32 -0300

    Resolução, falta colocar em ordem

--- contaPalavras.js
+++ contaPalavras.js
@@ -1,16 +1,28 @@
 // ##### Contador de Palavras #####
 /* Esse desafio tem por objetivo contar quantas palavras se repetemem cada frase. */
 
+var contagem = {};
+
 function contaPalavras(string){
     let arrumaFrase = string.replace(/\W/gm, " "); // Substitui todas as pontuações e acentuações
-    let fraseSeparada = arrumaFrase.split(/\s+/gm);
-    if (fraseSeparada[fraseSeparada.length - 1] == "") {
-        fraseSeparada.pop();        
+    let tokens = arrumaFrase.split(/\s+/gm);
+    if (tokens[tokens.length - 1] == "") {
+        tokens.pop();        
     }
-    console.log(fraseSeparada);
-    console.log(fraseSeparada.length);
+    console.log(tokens);
+    for (let i = 0; i < tokens.length; i++) {
+        var palavra = tokens[i].toLowerCase();
+        if (contagem[palavra] === undefined){
+            contagem[palavra] = 1;
+        } else {
+            contagem[palavra] = contagem[palavra] + 1;
+        }
+        
+    }
 }
 
-let frase = "Ao surgir, a! manha!";
+let frase = "JavaScript (ou 'JS') e uma linguagem de programaçao utilizada principalmente para scripts dinamicos do lado do cliente em paginas web, podendo tambem ser utilizada no lado do servidor, usando um interpretador (em ingles: runtime) como o Node.js.";
 
 contaPalavras(frase);
+
+console.table(contagem);
